66e8948c2111d7bc30559220275e3bb720fdec26
jiteshsrivastava <jiteshsri99@gmail.com>
2023-08-25 23:04:20 +0530

    for photo update

--- src/components/staticPages/gallery/Gallery.js
+++ src/components/staticPages/gallery/Gallery.js
@@ -1,11 +1,33 @@
 import { Container, Col, Card, Row, Carousel } from "react-bootstrap";
+import { FacebookEmbed } from 'react-social-media-embed';
 
 export default function Gallery() {
 	return (
 		<Container>
 			<div class="jumbotron">
-				<h1 class="display-4">Gallery</h1>
+				<h3 class="display-4">Gallery</h3>
 			</div>
+			<div style={{ display: 'flex', justifyContent: 'center' }}>
+				<div id="fb-root"></div>
+				<FacebookEmbed width={0} />
+				<script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v17.0" nonce="ooveFoif"></script>
+
+				<Row xs="1" md="2" className="g-4">
+					<Col>
+						<div class="fb-post" data-href="https://www.facebook.com/permalink.php?story_fbid=152556934551412&amp;id=100093913191887&amp;ref=embed_post" data-width="500" data-show-text="true"><blockquote cite="https://www.facebook.com/permalink.php?story_fbid=152556934551412&amp;id=100093913191887" class="fb-xfbml-parse-ignore"><p>VEGETABLE DAY CELEBRATE ON 19/08/2023 (PRE. NUR TO KG)</p>Posted by <a href="https://www.facebook.com/people/Gurukul-Academy/100093913191887/">Gurukul Academy</a> on&nbsp;<a href="https://www.facebook.com/permalink.php?story_fbid=152556934551412&amp;id=100093913191887">Saturday, 19 August 2023</a></blockquote></div>
+					</Col>
+					<Col>
+						<div class="fb-video" data-href="https://www.facebook.com/100093913191887/videos/317184197436908/" data-width="500" data-show-text="true"><blockquote cite="https://www.facebook.com/100093913191887/videos/317184197436908/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/100093913191887/videos/317184197436908/"></a><p>PROGRAM ON 15/08/2023</p>Posted by <a href="https://www.facebook.com/people/Gurukul-Academy/100093913191887/">Gurukul Academy</a> on Tuesday, 15 August 2023</blockquote></div>
+					</Col>
+					<Col>
+						<div class="fb-video" data-href="https://www.facebook.com/100093913191887/videos/746576240570702/" data-width="500" data-show-text="true"><blockquote cite="https://www.facebook.com/100093913191887/videos/746576240570702/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/100093913191887/videos/746576240570702/"></a><p>15/AUG/2023 PROGRAM CELEBRATE</p>Posted by <a href="https://www.facebook.com/people/Gurukul-Academy/100093913191887/">Gurukul Academy</a> on Tuesday, 15 August 2023</blockquote></div>
+					</Col>
+					<Col>
+						<div class="fb-video" data-href="https://www.facebook.com/100093913191887/videos/1434039817389706/" data-width="500" data-show-text="true"><blockquote cite="https://www.facebook.com/100093913191887/videos/1434039817389706/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/100093913191887/videos/1434039817389706/"></a><p>INDEPENDENCE CELEBRATE PROGRAM ON 15/08/2023</p>Posted by <a href="https://www.facebook.com/people/Gurukul-Academy/100093913191887/">Gurukul Academy</a> on Tuesday, 15 August 2023</blockquote></div>
+					</Col>
+				</Row>
+			</div>
+			{/*
 
 			<Row xs="1" md="2" className="g-4">
 
@@ -38,6 +60,9 @@ export default function Gallery() {
 					</Card>
 				</Col>				
 			</Row>
+
+	*/}
+
 		</Container>
 	);
 }
